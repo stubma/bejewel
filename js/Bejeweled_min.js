344042a8f242a7614414625e2873b6b8f8abe168
luma <stubma@gmail.com>
2013-04-09 16:38:06 +0800

    Game.CurvedValTable

--- js/Bejeweled_min.js
+++ js/Bejeweled_min.js
@@ -2300,12 +2300,12 @@ GameFramework.CurvedVal.prototype = {
 	IH : 0,
 	Xh : 0,
 	eL : function(b, c) {
-		this.me(Game.cd[b], c);
+		this.me(Game.CurvedValTable[b], c);
 		this.LC = b;
 		return this
 	},
 	vj : function(b) {
-		this.me(Game.cd[b], null);
+		this.me(Game.CurvedValTable[b], null);
 		this.LC = b;
 		return this
 	},
@@ -21591,43 +21591,43 @@ T(function() {
 U(function() {
 			Game.CrystalBall.c()
 		});
-Game.cd = t();
-Game.cd.prototype = {};
-Game.cd.c = function() {
-	Game.cd.Board_cs_02_09_11__17_30_09_511 = "b+0,1,0.005,1,#### %n###   N<###     P~###";
-	Game.cd.Board_cs_02_09_11__18_28_41_069 = "b+0,0,0,1,####         ~U###";
-	Game.cd.Board_cs_02_10_11__17_45_37_203 = "b#0,1,0.005,1,# VP  @6      -#";
-	Game.cd.EndLevelDialog_cs_11_04_11__13_54_55_550 = "b;0,1,0.01,0.3,~pMZ         ~D###";
-	Game.cd.EndLevelDialog_cs_11_04_11__15_01_52_045 = "b+0,1,0.01,0.3,G*ak         ~~###";
-	Game.cd.EndLevelDialog_cs_11_07_11__19_27_27_965 = "b;0,2,0.01,1.2,P###    NP###%P)(^  |W###  RPCi|";
-	Game.cd.Hyperspace_cs_11_10_11__11_45_36_674 = "b-0,1,0.0025,1,~###     M~### b#### S####y~###D~###";
-	Game.cd.Hyperspace_cs_11_10_11__12_06_32_324 = "b+0,1,0,1,~###       N~###  R)###";
-	Game.cd.Hyperspace_cs_11_10_11__12_08_47_105 = "b+0,1,0,1,~###     w~###   >####j####";
-	Game.cd.Board_cs_11_09_11__15_41_50_476 = "b;0,1,0.01,0.5,~###         ~####";
-	Game.cd.Board_cs_11_09_11__15_42_57_607 = "b;0,0,0,0.5,####         ~~###";
-	Game.cd.TutorialMgr_cs_11_11_11__15_19_51_707 = "b;0,1,0.01,0.15,#0F>         ~~###";
-	Game.cd.BejApp_cs_11_14_11__13_34_40_701 = "b+0,0,0,1,_###    [C### |o###   G_###";
-	Game.cd.HintDialog_cs_11_14_11__18_37_44_846 = "b;0,1,0.01,0.2,#-mh         ~~###";
-	Game.cd.HintDialog_cs_11_14_11__18_38_10_200 = "b;0,1,0.01,0.5,~s9y         ~####";
-	Game.cd.TutorialMgr_cs_11_14_11__18_50_05_912 = "b;0,1,0.01,1,~###         ~####";
-	Game.cd.TutorialMgr_cs_11_14_11__19_05_53_409 = "b;0,1,0.01,0.2,~of_         ~#M>5";
-	Game.cd.SpeedBoard_cs_11_15_11__17_35_36_339 = "b+0,1,0.01,5,####    t#### *~###    &~###";
-	Game.cd.ClassicBoard_cs_11_21_11__05_39_07_684 = "b+0,1,0.01,1,~###         ~####";
-	Game.cd.BejApp_cs_11_21_11__05_46_54_679 = "b+0,1,0.007692,1,~###         ~####";
-	Game.cd.SpeedBoard_cs_11_21_11__05_51_47_583 = "b+0,1,0.01,1,~###         ~####";
-	Game.cd.SpeedBoard_cs_11_21_11__05_58_39_924 = "b+0,1,0.02,1,~###         ~####";
-	Game.cd.DialogMgr_cs_11_28_11__18_12_06_572 = "b;0,1,0.01,0.4,#.<s         ~~###";
-	Game.cd.DialogMgr_cs_11_28_11__18_12_32_358 = "b;0,1,0.01,0.15,~pia         ~####";
-	Game.cd.MainMenu_cs_11_29_11__14_35_51_418 = "b;0,1,0.01,0.4,####         ~~###";
-	Game.cd.MainMenu_cs_11_29_11__14_37_36_539 = "b;0,1,0.01,0.4,~###         ~####";
-	Game.cd.MainMenu_cs_11_21_11__15_34_43_243 = "b;0,1,0.01,2,####    }####     $~###";
-	Game.cd.TutorialMgr_cs_11_14_11__19_18_09_946 = "b;0,1,0.01,0.1,~###         ~#B}2"
+Game.CurvedValTable = t();
+Game.CurvedValTable.prototype = {};
+Game.CurvedValTable.c = function() {
+	Game.CurvedValTable.Board_cs_02_09_11__17_30_09_511 = "b+0,1,0.005,1,#### %n###   N<###     P~###";
+	Game.CurvedValTable.Board_cs_02_09_11__18_28_41_069 = "b+0,0,0,1,####         ~U###";
+	Game.CurvedValTable.Board_cs_02_10_11__17_45_37_203 = "b#0,1,0.005,1,# VP  @6      -#";
+	Game.CurvedValTable.EndLevelDialog_cs_11_04_11__13_54_55_550 = "b;0,1,0.01,0.3,~pMZ         ~D###";
+	Game.CurvedValTable.EndLevelDialog_cs_11_04_11__15_01_52_045 = "b+0,1,0.01,0.3,G*ak         ~~###";
+	Game.CurvedValTable.EndLevelDialog_cs_11_07_11__19_27_27_965 = "b;0,2,0.01,1.2,P###    NP###%P)(^  |W###  RPCi|";
+	Game.CurvedValTable.Hyperspace_cs_11_10_11__11_45_36_674 = "b-0,1,0.0025,1,~###     M~### b#### S####y~###D~###";
+	Game.CurvedValTable.Hyperspace_cs_11_10_11__12_06_32_324 = "b+0,1,0,1,~###       N~###  R)###";
+	Game.CurvedValTable.Hyperspace_cs_11_10_11__12_08_47_105 = "b+0,1,0,1,~###     w~###   >####j####";
+	Game.CurvedValTable.Board_cs_11_09_11__15_41_50_476 = "b;0,1,0.01,0.5,~###         ~####";
+	Game.CurvedValTable.Board_cs_11_09_11__15_42_57_607 = "b;0,0,0,0.5,####         ~~###";
+	Game.CurvedValTable.TutorialMgr_cs_11_11_11__15_19_51_707 = "b;0,1,0.01,0.15,#0F>         ~~###";
+	Game.CurvedValTable.BejApp_cs_11_14_11__13_34_40_701 = "b+0,0,0,1,_###    [C### |o###   G_###";
+	Game.CurvedValTable.HintDialog_cs_11_14_11__18_37_44_846 = "b;0,1,0.01,0.2,#-mh         ~~###";
+	Game.CurvedValTable.HintDialog_cs_11_14_11__18_38_10_200 = "b;0,1,0.01,0.5,~s9y         ~####";
+	Game.CurvedValTable.TutorialMgr_cs_11_14_11__18_50_05_912 = "b;0,1,0.01,1,~###         ~####";
+	Game.CurvedValTable.TutorialMgr_cs_11_14_11__19_05_53_409 = "b;0,1,0.01,0.2,~of_         ~#M>5";
+	Game.CurvedValTable.SpeedBoard_cs_11_15_11__17_35_36_339 = "b+0,1,0.01,5,####    t#### *~###    &~###";
+	Game.CurvedValTable.ClassicBoard_cs_11_21_11__05_39_07_684 = "b+0,1,0.01,1,~###         ~####";
+	Game.CurvedValTable.BejApp_cs_11_21_11__05_46_54_679 = "b+0,1,0.007692,1,~###         ~####";
+	Game.CurvedValTable.SpeedBoard_cs_11_21_11__05_51_47_583 = "b+0,1,0.01,1,~###         ~####";
+	Game.CurvedValTable.SpeedBoard_cs_11_21_11__05_58_39_924 = "b+0,1,0.02,1,~###         ~####";
+	Game.CurvedValTable.DialogMgr_cs_11_28_11__18_12_06_572 = "b;0,1,0.01,0.4,#.<s         ~~###";
+	Game.CurvedValTable.DialogMgr_cs_11_28_11__18_12_32_358 = "b;0,1,0.01,0.15,~pia         ~####";
+	Game.CurvedValTable.MainMenu_cs_11_29_11__14_35_51_418 = "b;0,1,0.01,0.4,####         ~~###";
+	Game.CurvedValTable.MainMenu_cs_11_29_11__14_37_36_539 = "b;0,1,0.01,0.4,~###         ~####";
+	Game.CurvedValTable.MainMenu_cs_11_21_11__15_34_43_243 = "b;0,1,0.01,2,####    }####     $~###";
+	Game.CurvedValTable.TutorialMgr_cs_11_14_11__19_18_09_946 = "b;0,1,0.01,0.1,~###         ~#B}2"
 };
 T(function() {
-			Game.cd.u("Game.CurvedValTable", null)
+			Game.CurvedValTable.u("Game.CurvedValTable", null)
 		});
 U(function() {
-			Game.cd.c()
+			Game.CurvedValTable.c()
 		});
 Game.QD = function(b, c, d) {
 	this.xa = b;
